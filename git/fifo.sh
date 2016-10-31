e64b80cb0220473c0c73a8c2729810a99beda8d9
Broskoll <broskoll.inc@gmail.com>
2016-10-31 18:18:46 +0100

    Update fifo.sh

--- git/fifo.sh
+++ git/fifo.sh
@@ -2,7 +2,7 @@
 #wget -O fifo.sh goo.gl/2T0oQ4 && bash fifo.sh
 
 sudo apt-get update
-sudo apt-get upgrade
+#sudo apt-get upgrade
 sudo passwd
 wget -O fifo2.sh goo.gl/WcDa8H
 wget -O siso.sh goo.gl/o5OEc7
