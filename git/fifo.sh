1f94c3be8dc2f14564119f384c61783d9c5eec95
Broskoll <broskoll.inc@gmail.com>
2016-10-31 16:15:23 +0100

    Update fifo.sh

--- git/fifo.sh
+++ git/fifo.sh
@@ -1,7 +1,4 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo passwd
-su
-echo "/dev/sda1 /home/git/usb vfat uid=git,gid=git,umask=0022,sync,auto,nosuid,rw,nouser 0 0" >> /etc/fstab
-echo "git   ALL = NOPASSWD: ALL" >> /etc/sudoers
-reboot
+su root ~/fifo2.sh
