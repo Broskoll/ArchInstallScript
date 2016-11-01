975004492e319e53c67a082cb46f60b16406b582
Broskoll <broskoll.inc@gmail.com>
2016-11-01 15:52:19 +0100

    Update siso.sh

--- git/siso.sh
+++ git/siso.sh
@@ -4,4 +4,7 @@ sudo mkdir /home/git/usb/lifeup.git
 cd /home/git/usb/lifeup.git
 sudo git init --bare
 
+wget http://http.debian.net/debian/pool/main/libg/libgit2/libgit2_0.24.1.orig.tar.gz
+
 git clone git://git.2f30.org/stagit.git
+cd stagit
