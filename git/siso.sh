5291e7736bb6f29e39734deaee0a2c5872bd0e90
Broskoll <broskoll.inc@gmail.com>
2016-11-01 12:39:37 +0100

    Update siso.sh

--- git/siso.sh
+++ git/siso.sh
@@ -1,8 +1,5 @@
-sudo nano /etc/sudoers //add git user some rights
-//sudo raspi-config //Expand filesystem, autologin in terminal and enable ssh
-cd /etc/systemd/system/
-sudo nano auto* //Change pi by git
-mkdir usb
-sudo nano /etc/fstab //add at the end : /dev/sda1 /home/git/usb vfat uid=git,gid=git,umask=0022,sync,auto,nosuid,rw,nouser 0 0
-cd /home/git/usb/git.git
+#!/bin/bash
+
+mkdir /home/git/lifeup.git
+cd /home/git/lifeup.git
 git init --bare
