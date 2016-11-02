683a4855b9ba61c2fccad2dbf424f155d243963c
Broskoll <broskoll.inc@gmail.com>
2016-11-02 14:27:02 +0100

    Update fifo.sh

--- gogs/fifo.sh
+++ gogs/fifo.sh
@@ -5,8 +5,11 @@ sudo apt-get update
 #sudo apt-get upgrade -y
 echo -e "Root passwd : "
 sudo passwd
+sudo adduser --disabled-login --gecos 'Gogs' git
 echo -e "Pi passwd : "
 passwd pi
+echo -e "Git passwd : "
+sudo passwd git
 wget https://raw.githubusercontent.com/Broskoll/Setup/master/gogs/fifo2.sh
 wget https://raw.githubusercontent.com/Broskoll/Setup/master/gogs/siso.sh
 su root ~/fifo2.sh
