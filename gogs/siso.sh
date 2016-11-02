4ffcf4402f0df376d1059217bbe53dafe1a785ec
Broskoll <broskoll.inc@gmail.com>
2016-11-02 13:49:44 +0100

    Update siso.sh

--- gogs/siso.sh
+++ gogs/siso.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "/dev/sda1 /home/pi/usb vfat uid=git,gid=git,umask=0022,sync,auto,nosuid,rw,nouser 0 0" >> /etc/fstab
+echo "/dev/sda1 /home/pi/usb vfat uid=pi,gid=pi,umask=0022,sync,auto,nosuid,rw,nouser 0 0" >> /etc/fstab
 reboot
