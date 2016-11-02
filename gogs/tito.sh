802c4a8696dad141857b22b7d4eac68ec5800707
Broskoll <broskoll.inc@gmail.com>
2016-11-02 12:40:16 +0100

    Create tito.sh

---
+++ gogs/tito.sh
@@ -0,0 +1,12 @@
+sudo apt-get install git
+wget https://dl.gogs.io/gogs_latest_raspi2.zip
+unzip gogs_latest_raspi2.zip
+sudo cp /home/git/gogs/scripts/systemd/gogs.service /etc/systemd/system/
+sudo systemctl enable gogs
+sudo systemctl start gogs
+
+#If config change
+#https://gogs.io/docs/advanced/configuration_cheat_sheet
+#custom/conf/app.ini
+#sudo systemctl stop gogs
+#sudo systemctl start gogs
