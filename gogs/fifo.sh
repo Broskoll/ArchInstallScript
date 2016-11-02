9f57f01cd89688fc9c74956abfa36ddfebea3f97
Broskoll <broskoll.inc@gmail.com>
2016-11-02 12:38:23 +0100

    Update fifo.sh

--- gogs/fifo.sh
+++ gogs/fifo.sh
@@ -5,11 +5,9 @@ sudo apt-get update
 #sudo apt-get upgrade
 echo -e "Root passwd : "
 sudo passwd
-sudo adduser --disabled-login --gecos 'Gogs' git
 echo -e "Pi passwd : "
 passwd pi
 wget https://raw.githubusercontent.com/Broskoll/Setup/master/gogs/fifo2.sh
-wget https://raw.githubusercontent.com/Broskoll/Setup/master/gogs/siso.sh
 su root ~/fifo2.sh
 sudo apt-get install git
 wget https://dl.gogs.io/gogs_latest_raspi2.zip
