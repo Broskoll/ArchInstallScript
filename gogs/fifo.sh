edc60949bcca1f68f02d341bac8b7b2720bae424
Broskoll <broskoll.inc@gmail.com>
2016-11-02 12:37:30 +0100

    Update fifo.sh

--- gogs/fifo.sh
+++ gogs/fifo.sh
@@ -12,3 +12,14 @@ wget https://raw.githubusercontent.com/Broskoll/Setup/master/gogs/fifo2.sh
 wget https://raw.githubusercontent.com/Broskoll/Setup/master/gogs/siso.sh
 su root ~/fifo2.sh
 sudo apt-get install git
+wget https://dl.gogs.io/gogs_latest_raspi2.zip
+unzip gogs_latest_raspi2.zip
+sudo cp /home/git/gogs/scripts/systemd/gogs.service /etc/systemd/system/
+sudo systemctl enable gogs
+sudo systemctl start gogs
+
+#If config change
+#https://gogs.io/docs/advanced/configuration_cheat_sheet
+#custom/conf/app.ini
+#sudo systemctl stop gogs
+#sudo systemctl start gogs
