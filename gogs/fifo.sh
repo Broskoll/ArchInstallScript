890da3d430b0f0a70c1716bfa2d0c64e2948ceee
Broskoll <broskoll.inc@gmail.com>
2016-11-02 12:40:05 +0100

    Update fifo.sh

--- gogs/fifo.sh
+++ gogs/fifo.sh
@@ -8,16 +8,5 @@ sudo passwd
 echo -e "Pi passwd : "
 passwd pi
 wget https://raw.githubusercontent.com/Broskoll/Setup/master/gogs/siso.sh
-su root ~/fifo2.sh
-sudo apt-get install git
-wget https://dl.gogs.io/gogs_latest_raspi2.zip
-unzip gogs_latest_raspi2.zip
-sudo cp /home/git/gogs/scripts/systemd/gogs.service /etc/systemd/system/
-sudo systemctl enable gogs
-sudo systemctl start gogs
-
-#If config change
-#https://gogs.io/docs/advanced/configuration_cheat_sheet
-#custom/conf/app.ini
-#sudo systemctl stop gogs
-#sudo systemctl start gogs
+wget https://raw.githubusercontent.com/Broskoll/Setup/master/gogs/tito.sh
+su root ~/siso.sh
