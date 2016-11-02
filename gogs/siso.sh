a57942b7804d70ae8ab434e6c1cb6b13b44b93a1
Broskoll <broskoll.inc@gmail.com>
2016-11-02 15:06:08 +0100

    Update siso.sh

--- gogs/siso.sh
+++ gogs/siso.sh
@@ -1,4 +1,4 @@
-sudo apt-get install git
+sudo apt-get install git -y
 wget https://raw.githubusercontent.com/Broskoll/Setup/master/gogs/tito.sh
 sudo mv tito.sh /home/git/
 sudo su - git /home/git/tito.sh
