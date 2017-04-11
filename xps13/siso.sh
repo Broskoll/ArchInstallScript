c3740a92753744165c8bcc4b4e25e2ade98d5333
Broskoll <broskoll.inc@gmail.com>
2017-04-11 12:28:35 +0100

    Update siso.sh

--- xps13/siso.sh
+++ xps13/siso.sh
@@ -14,7 +14,7 @@ sudo pacman -S `#comment` \
 xf86-video-intel mesa `#Core` \
 xorg-server xorg-server-utils xorg-xinit \
 `linux-zen linux-zen-headers \`#Kernel\`` \
-xf86-input-synaptics xf86-input-libinput \
+xf86-input-synaptics xf86-input-libinput `#Touchpad` \
 termite `#Terminal` \
 neovim `#Text editor` \
 firefox `#Browser` \
